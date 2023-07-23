9f216a2a25a9541d181406c6be9021f9e0de1809
anilkumarnagaraj <anilkumar@in.ibm.com>
2023-07-23 20:52:37 +0530

    Update main.tf

--- examples/read_bucket/main.tf
+++ examples/read_bucket/main.tf
@@ -3,9 +3,21 @@
 # Copyright 2020 IBM
 #####################################################
 
-data "ibm_cos_bucket" "cos_bucket" {
-  bucket_name          = var.bucket_name
-  resource_instance_id = var.cos_instance_id  
-  bucket_type          = "single_site_location"
-  bucket_region        = var.location
+resource "time_sleep" "wait_3_seconds" {
+  create_duration = "2500s"
 }
+
+resource "null_resource" "topic_events" {
+  triggers = {
+    always_run = timestamp()
+    topic = "rmq"
+  }
+  depends_on = [time_sleep.wait_3_seconds]
+}
+
+#data "ibm_cos_bucket" "cos_bucket" {
+#  bucket_name          = var.bucket_name
+#  resource_instance_id = var.cos_instance_id  
+#  bucket_type          = "single_site_location"
+#  bucket_region        = var.location
+#}
