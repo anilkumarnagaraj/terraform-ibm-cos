a94863584a8baf1682c96c6ec9e8e2b027c5279c
Cocoa <cocoa@hu.ibm.com>
2023-08-31 19:47:33 +0530

    add sample terraform file

---
+++ examples/sample/main.tf
@@ -0,0 +1,16 @@
+#####################################################
+# Sample
+# Copyright 2020 IBM
+#####################################################
+
+resource "time_sleep" "wait_seconds" {
+  create_duration = "10s"
+}
+
+resource "null_resource" "topic_events_1" {
+  triggers = {
+    always_run = timestamp()
+    topic = "rmq"
+  }
+  depends_on = [time_sleep.wait_seconds]
+}
