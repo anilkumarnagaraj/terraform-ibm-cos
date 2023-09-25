bf8ed74996586878a7ea1611a940539d343d4d93
Cocoa <cocoa@hu.ibm.com>
2023-09-25 20:50:52 +0530

    add vpc code

---
+++ examples/vpc_cbr/variables.tf
@@ -0,0 +1,10 @@
+#####################################################
+# COS Bucket
+# Copyright 2020 IBM
+#####################################################
+
+variable "vpc_name" {
+    description = "Name of the vpc"
+    type        = string
+    default =   "test"
+}
