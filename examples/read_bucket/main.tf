dd7b54707e815130126c2255aa1d693f22a072c5
anilkumarnagaraj <anilkumar@in.ibm.com>
2023-10-13 06:54:58 +0530

    Update main.tf

--- examples/read_bucket/main.tf
+++ examples/read_bucket/main.tf
@@ -5,22 +5,22 @@
 
 
 data "ibm_resource_group" "cos_group" {
-  name = "Default"
+  name = var.resource_group
 }
 
 data "ibm_resource_instance" "cos_instance" {
-  name              = "Cloud Object Storage-e9"
+  name              = var.bucket_name
   resource_group_id = data.ibm_resource_group.cos_group.id
   service           = "cloud-object-storage"
 }
 
-data "ibm_cos_bucket" "standard-eu-de" {
-  bucket_name          = "sch-bcr-cos"
+data "ibm_cos_bucket" "read-bucket" {
+  bucket_name          = var.bucket_name
   resource_instance_id = data.ibm_resource_instance.cos_instance.id
   bucket_type          = "single_site_location"
-  bucket_region        = "eu-de"
+  bucket_region        = var.location
 }
 
 output "bucket_private_endpoint" {
-  value = data.ibm_cos_bucket.standard-eu-de.s3_endpoint_private
+  value = data.ibm_cos_bucket.read-bucket.s3_endpoint_private
 }
