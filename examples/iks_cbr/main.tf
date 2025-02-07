6eee2f45dbab0c9dbe82b37bfc6d2a0830548545
anilkumarnagaraj <anilkumar@in.ibm.com>
2025-02-07 08:29:15 +0530

    Update main.tf

--- examples/iks_cbr/main.tf
+++ examples/iks_cbr/main.tf
@@ -6,3 +6,8 @@
 data "ibm_container_vpc_cluster" "cluster" {
   name = var.cluster_name_id
 }
+
+data "ibm_container_cluster_config" "cluster_config {
+  cluster_name_id = var.cluster_name_id
+  admin           = true
+}
