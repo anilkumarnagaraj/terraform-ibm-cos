50a83cb0869bed4f297a5b1e9c2270519ccd0033
anilkumarnagaraj <anilkumar@in.ibm.com>
2025-09-18 14:16:21 +0530

    Update main.tf

--- examples/iks_cbr/main.tf
+++ examples/iks_cbr/main.tf
@@ -3,10 +3,10 @@
 # Copyright 2020 IBM
 #####################################################
 
-data "ibm_container_vpc_cluster" "cluster" {
-  name = var.cluster_name_id
-  resource_group_id = var.resource_group_id
-}
+#data "ibm_container_vpc_cluster" "cluster" {
+#  name = var.cluster_name_id
+#  resource_group_id = var.resource_group_id
+#}
 
 data "ibm_container_cluster_config" "cluster_config" {
   cluster_name_id = var.cluster_name_id
